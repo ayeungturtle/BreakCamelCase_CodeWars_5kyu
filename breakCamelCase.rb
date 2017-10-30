162aa24903393625319835fd7eadaa21abe7ff27
AndrewGit <andrewbryantyeung@gmail.com>
2017-10-30 16:39:15 -0400

    figured out strange upper check syntax

--- breakCamelCase.rb
+++ breakCamelCase.rb
@@ -1,4 +1,10 @@
 def solution(string)
+    string.each_char do |i|
+        if /[[:upper:]]/ =~ i
+            puts i
+        end
+    end
     # list will allow me to check if letter is upper /[[:upper:]]/ 
 end
 
+solution ('sgIlsgI:NKsg')
